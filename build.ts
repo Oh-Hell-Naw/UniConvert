4c1ecf46b2717bcf1ada10ec8cc0f6f11057db2c
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-12-27 03:26:17 +0100

    Update build.ts

--- build.ts
+++ build.ts
@@ -5,6 +5,7 @@ await command.output();
 
 const hash = createHash("sha256");
 const file = await Deno.open("uniconvert.exe");
+// deno-lint-ignore no-deprecated-deno-api
 for await (const chunk of Deno.iter(file)) hash.update(chunk);
 const currentHash = hash.toString().toUpperCase();
 Deno.close(file.rid);
